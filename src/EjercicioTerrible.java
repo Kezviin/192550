4077a3809d151a8a27f136f67ff87a7d210aeb4f
kezvin <kevinlibardorosas@gmail.com>
2025-03-21 13:47:14 -0500

    Se crea estructura java del ejercicio de mantenimiento

--- src/EjercicioTerrible.java
+++ src/EjercicioTerrible.java
@@ -2,72 +2,31 @@ import java.util.Scanner;
 
 public class EjercicioTerrible {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-
-        System.out.println("Ingrese el nombre del primer estudiante:");
-        String a = x.nextLine();
-        System.out.println("Ingrese la edad:");
-        int b = x.nextInt();
-        x.nextLine();
-
-        System.out.println("Ingrese el nombre del segundo estudiante:");
-        String c = x.nextLine();
-        System.out.println("Ingrese la edad:");
-        float d = x.nextFloat();
-        x.nextLine();
-
-        System.out.println("Ingrese el nombre del tercer estudiante:");
-        String e = x.nextLine();
-        System.out.println("Ingrese la edad:");
-        double f = x.nextDouble();
-        x.nextLine();
-
-        System.out.println("Ingrese el nombre del cuarto estudiante:");
-        String g = x.nextLine();
-        System.out.println("Ingrese la edad:");
-        short h = x.nextShort();
-        x.nextLine();
-
-        System.out.println("Ingrese el nombre del quinto estudiante:");
-        String i = x.nextLine();
-        System.out.println("Ingrese la edad:");
-        byte j = x.nextByte();
-        x.nextLine();
-
-        double y = (b + d + f + h + j) / 5;
-
-        System.out.println("Edad promedio de los estudiantes: " + y);
-
-        if (b > 17) {
-            System.out.println(a + " es mayor de edad");
-        } else {
-            System.out.println(a + " es menor de edad");
+        Scanner escanearNumeros = new Scanner(System.in);
+        Scanner escanearNombres = new Scanner(System.in);
+            float edades=0;
+            float promedioEdad=0;
+        for (int i = 1; i <=5; i++) {
+            System.out.println("Ingrese el nombre del estudiante n°" + i);
+            String nombre = escanearNombres.nextLine();
+            System.out.println("el nombre del estudiante N°" + i + " es :" + nombre);
+            System.out.println("Ingrese la edad del estudiante n° " + i);
+            byte edad = escanearNumeros.nextByte();
+            edades = edades+edad;
+            if (edad>17) {
+                System.out.println("el estudiante n° " + i +" es mayor de edad");
+            } else {
+                System.out.println("el estudiante n° " + i +" es menor de edad");
+            }
+            if (i==5) {
+                promedioEdad=(edades)/5;
+                System.out.println("el promedio de las edades de los estudiantes es: " + promedioEdad);
+                System.out.println("Programa finalizado.");
+            }
         }
+        escanearNombres.close();
+        escanearNumeros.close();
 
-        if (d > 17) {
-            System.out.println(c + " es mayor de edad");
-        } else {
-            System.out.println(c + " es menor de edad");
-        }
-
-        if (f > 17) {
-            System.out.println(e + " es mayor de edad");
-        } else {
-            System.out.println(e + " es menor de edad");
-        }
-
-        if (h > 17) {
-            System.out.println(g + " es mayor de edad");
-        } else {
-            System.out.println(g + " es menor de edad");
-        }
-
-        if (j > 17) {
-            System.out.println(i + " es mayor de edad");
-        } else {
-            System.out.println(i + " es menor de edad");
-        }
-
-        System.out.println("Programa finalizado.");
     }
+    
 }
